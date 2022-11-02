707bfcc0287fc724bdfc0a0953f3b5ffc5e035af
dylan4d <forde.dylan@gmail.com>
2022-11-02 19:45:03 +0000

    Second constructor with rgb array and test.

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Colour {
     double red;
     double green;
     double blue;
+    int[] rgb;
+    int[] convert_red;
+    int[] convert_green;
+    int[] convert_blue;
 
     public Colour(double red, double green, double blue) {
         this.red = red;
@@ -11,6 +18,16 @@ public class Colour {
         if ((red < 0 || red > 1) || (green < 0 || green > 1) || (blue < 0 || blue > 1)){
             throw new IllegalArgumentException();
         }
+    }
+
+    public Colour(int[] rgb) {
+        this.rgb = rgb;
+
+        this.convert_blue = Arrays.copyOfRange(rgb, 0, 7);
+        this.convert_green = Arrays.copyOfRange(rgb, 8, 15);
+        this.convert_red = Arrays.copyOfRange(rgb, 16, 23);
+
+
     }
 
 }
--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -1,6 +1,6 @@
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 public class ColourTest {
 
@@ -14,5 +14,16 @@ public class ColourTest {
         Colour no_over_one = new Colour(1.4, 1.5, 0.2);
     }
 
+    @Test
+    public void combined_constructor(){
+        // Blue[0:7], Green[8:15], Red[7:15]
+        int[] rgb_val = new int[24];
+
+        for (int i = 0; i < 24; i++) {
+            rgb_val[i] = 1;
+        }
+
+        Colour combined_values = new Colour(rgb_val);
+    }
 
 }
