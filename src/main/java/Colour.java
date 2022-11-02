6e75d9cabd69e886ed100863de04ee60379c303d
dylan4d <forde.dylan@gmail.com>
2022-11-02 22:06:32 +0000

    Refactoring changes after all tests have passed.

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -1,23 +1,19 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-
-import static java.lang.Math.round;
-
 public class Colour {
-    double red;
-    double green;
-    double blue;
-    int[] rgb = new int[24];
-    int[] convert_red;
-    int[] convert_green;
-    int[] convert_blue;
+    private double red;
+    private double green;
+    private double blue;
+    private int[] rgb = new int[24];
+    private int[] convert_red;
+    private int[] convert_green;
+    private int[] convert_blue;
 
     public Colour(double r, double g, double b) {
-        this.red = Math.round(r * 255);
-        this.green = Math.round(g * 255);
-        this.blue = Math.round(b * 255);
+        this.setRed(Math.round(r * 255));
+        this.setGreen(Math.round(g * 255));
+        this.setBlue(Math.round(b * 255));
 
-        if ((this.red < 0 || this.red > 255) || (this.green < 0 || this.green > 255) || (this.blue < 0 || this.blue > 255)){
+        if ((this.getRed() < 0 || this.getRed() > 255) || (this.getGreen() < 0 || this.getGreen() > 255) || (this.getBlue() < 0 || this.getBlue() > 255)){
             throw new IllegalArgumentException("Decimal must be between 0 and 255");
         }
     }
@@ -34,33 +30,33 @@ public class Colour {
             }
         }
 
-        this.rgb = rgb;
+        this.setRgb(rgb);
 
-        this.convert_blue = Arrays.copyOfRange(rgb, 0, 8);
-        this.convert_green = Arrays.copyOfRange(rgb, 8, 16);
-        this.convert_red = Arrays.copyOfRange(rgb, 16, 24);
+        this.setConvert_blue(Arrays.copyOfRange(rgb, 0, 8));
+        this.setConvert_green(Arrays.copyOfRange(rgb, 8, 16));
+        this.setConvert_red(Arrays.copyOfRange(rgb, 16, 24));
 
         // how to convert int[] to num between 0 and 255
         StringBuilder toParseBlue = new StringBuilder();
-        for (int i: convert_blue) {
+        for (int i: getConvert_blue()) {
             toParseBlue.append(i);
         }
 
         StringBuilder toParseGreen = new StringBuilder();
-        for (int i: convert_green) {
+        for (int i: getConvert_green()) {
             toParseGreen.append(i);
         }
 
         StringBuilder toParseRed = new StringBuilder();
-        for (int i: convert_red) {
+        for (int i: getConvert_red()) {
             toParseRed.append(i);
         }
 
-        this.red = Integer.parseInt(toParseRed.toString(), 2);
-        this.blue = Integer.parseInt(toParseBlue.toString(), 2);
-        this.green = Integer.parseInt(toParseGreen.toString(), 2);
+        this.setRed(Integer.parseInt(toParseRed.toString(), 2));
+        this.setBlue(Integer.parseInt(toParseBlue.toString(), 2));
+        this.setGreen(Integer.parseInt(toParseGreen.toString(), 2));
 
-        if ((this.red < 0 || this.red > 255) || (this.green < 0 || this.green > 255) || (this.blue < 0 || this.blue > 255)){
+        if ((this.getRed() < 0 || this.getRed() > 255) || (this.getGreen() < 0 || this.getGreen() > 255) || (this.getBlue() < 0 || this.getBlue() > 255)){
             throw new IllegalArgumentException("Decimal must be between 0 and 255");
         }
 
@@ -82,9 +78,53 @@ public class Colour {
 
         if (obj instanceof Colour) {
             Colour col1 = (Colour) obj;
-            return (col1.red == this.red && col1.green == this.green && col1.blue == this.blue);
+            return (col1.getRed() == this.getRed() && col1.getGreen() == this.getGreen() && col1.getBlue() == this.getBlue());
         }
 
         return false;
+    }
+
+    public void setRed(double red) {
+        this.red = red;
+    }
+
+    public void setGreen(double green) {
+        this.green = green;
+    }
+
+    public void setBlue(double blue) {
+        this.blue = blue;
+    }
+
+    public int[] getConvert_red() {
+        return convert_red;
+    }
+
+    public void setConvert_red(int[] convert_red) {
+        this.convert_red = convert_red;
+    }
+
+    public int[] getConvert_green() {
+        return convert_green;
+    }
+
+    public void setConvert_green(int[] convert_green) {
+        this.convert_green = convert_green;
+    }
+
+    public int[] getConvert_blue() {
+        return convert_blue;
+    }
+
+    public void setConvert_blue(int[] convert_blue) {
+        this.convert_blue = convert_blue;
+    }
+
+    public int[] getRgb() {
+        return rgb;
+    }
+
+    public void setRgb(int[] rgb) {
+        this.rgb = rgb;
     }
 }
