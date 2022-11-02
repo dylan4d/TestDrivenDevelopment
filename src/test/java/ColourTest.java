fc8229fe4a0f9e7df5752bbcc7ddee6c37bcad1e
dylan4d <forde.dylan@gmail.com>
2022-11-02 17:05:34 +0000

    Added constructor test with no values over 1

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -9,4 +9,10 @@ public class ColourTest {
         Colour no_neg = new Colour(-0.6, 0.5, 0.4);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void no_values_over_one(){
+        Colour no_over_one = new Colour(1.4, 1.5, 0.2);
+    }
+
+
 }
