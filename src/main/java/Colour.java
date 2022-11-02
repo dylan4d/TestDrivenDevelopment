90a8c530141626aa8edef096079b743893c5d69a
dylan4d <forde.dylan@gmail.com>
2022-11-02 21:15:15 +0000

    improved naming across files

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -5,29 +5,68 @@ public class Colour {
     double red;
     double green;
     double blue;
-    int[] rgb;
+    int[] rgb = new int[24];
     int[] convert_red;
     int[] convert_green;
     int[] convert_blue;
 
-    public Colour(double red, double green, double blue) {
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
+    public Colour(double r, double g, double b) {
+        this.red = r * 255;
+        this.green = g * 255;
+        this.blue = b * 255;
 
-        if ((red < 0 || red > 1) || (green < 0 || green > 1) || (blue < 0 || blue > 1)){
-            throw new IllegalArgumentException();
+        if ((r < 0 || r > 1) || (g < 0 || g > 1) || (b < 0 || b > 1)){
+            throw new IllegalArgumentException("Incorrect Input");
         }
     }
 
     public Colour(int[] rgb) {
+
+        if (rgb.length != 24) {
+            throw new IllegalArgumentException("Incorrect Input");
+        }
+
         this.rgb = rgb;
 
-        this.convert_blue = Arrays.copyOfRange(rgb, 0, 7);
-        this.convert_green = Arrays.copyOfRange(rgb, 8, 15);
-        this.convert_red = Arrays.copyOfRange(rgb, 16, 23);
+        this.convert_blue = Arrays.copyOfRange(rgb, 0, 8);
+        this.convert_green = Arrays.copyOfRange(rgb, 8, 16);
+        this.convert_red = Arrays.copyOfRange(rgb, 16, 24);
 
+        // how to convert int[] to num between 0 and 255
+        StringBuilder toParseBlue = new StringBuilder();
+        for (int i: convert_blue) {
+            toParseBlue.append(i);
+        }
+
+        StringBuilder toParseGreen = new StringBuilder();
+        for (int i: convert_green) {
+            toParseGreen.append(i);
+        }
+
+        StringBuilder toParseRed = new StringBuilder();
+        for (int i: convert_red) {
+            toParseRed.append(i);
+        }
+
+        this.red = Integer.parseInt(toParseRed.toString(), 2);
+        this.blue = Integer.parseInt(toParseBlue.toString(), 2);
+        this.green = Integer.parseInt(toParseGreen.toString(), 2);
+
+        if ((this.red < 0 || this.red > 255) || (this.green < 0 || this.green > 255) || (this.blue < 0 || this.blue > 255)){
+            throw new IllegalArgumentException();
+        }
 
     }
 
+    public double getRed() {
+        return red;
+    }
+
+    public double getGreen() {
+        return green;
+    }
+
+    public double getBlue() {
+        return blue;
+    }
 }
--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -6,24 +6,31 @@ public class ColourTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void no_negative_values(){
-        Colour no_neg = new Colour(-0.6, 0.5, 0.4);
+
+        Colour no_below_one = new Colour(-0.6, 0.5, 0.4);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void no_values_over_one(){
-        Colour no_over_one = new Colour(1.4, 1.5, 0.2);
-    }
 
+        Colour no_over_one = new Colour(1.4, 1.5, 0.2);
+    };
+
+    @Test(expected = IllegalArgumentException.class)
+    public void no_too_long_array() {
+        Colour no_longer_array = new Colour(new int[25]);
+    }
     @Test
-    public void combined_constructor(){
-        // Blue[0:7], Green[8:15], Red[7:15]
-        int[] rgb_val = new int[24];
+    public void convert_from_int_array_to_correct_int(){
+
+        int[] white_val = new int[24];
 
         for (int i = 0; i < 24; i++) {
-            rgb_val[i] = 1;
+            white_val[i] = 1;
         }
 
-        Colour combined_values = new Colour(rgb_val);
+        Colour white2 = new Colour(white_val);
+        Assert.assertEquals(255.0, white2.getRed(), 0.01);
     }
 
 }
